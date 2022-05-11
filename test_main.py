4554b099a2f14955ca335fa431ce7bd3dd6af2ba
huydt-0234 <dinh.trong.huy@sun-asterisk.com>
2022-05-11 16:25:04 +0700

    multiply instead of add

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import hello, add
 def test_add():
     assert add(2, 3) == 5, "should be 5"
     print("Correctly")
-    assert Add(5,5) == 11
+    assert add(5,5) == 11
 
 def test_hello():
     assert hello("huy") == "Hello_huy", "should be Hello huy" 
